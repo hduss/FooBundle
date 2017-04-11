4910827a3dab85a1709850900ebc41678bf190f5
hduss <delmas.theo@laposte.net>
2017-04-11 11:17:55 +0200

    creation d'un nouveau bundle CustomerBundle, commentaire de FooBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             #new AppBundle\AppBundle(),
-            new FooBundle\FooBundle(),
+            #new FooBundle\FooBundle(),
+            new CustomerBundle\CustomerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
