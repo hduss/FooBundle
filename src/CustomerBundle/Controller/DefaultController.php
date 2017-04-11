ab462178fc23eb915f60edcafe64d58f869e9875
hduss <delmas.theo@laposte.net>
2017-04-11 13:17:17 +0200

    tous les templates OK

--- src/CustomerBundle/Controller/DefaultController.php
+++ src/CustomerBundle/Controller/DefaultController.php
@@ -9,26 +9,30 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('CustomerBundle:Default:index.html.twig');
+        return $this->render('CustomerBundle:base.html.twig');
     }
 
     public function createAction()
     {
-    	return new response('Created !!');
+    	//return new response('Created !!');
+    	return $this->render('CustomerBundle:Default:create.html.twig');
     }
 
     public function detailAction($identifiant)
     {
-    	return new response('Detail du client ' . $identifiant);
+    	//return new response('Detail du client ' . $identifiant);
+    	return $this->render('CustomerBundle:Default:detail.html.twig', ['']);
     }
 
-    public function updateAction()
+    public function updateAction($identifiant)
     {
-    	return new response('Update Client !');
+    	//return new response('Update du Client ' . $identifiant);
+    	return $this->render('CustomerBundle:Default:update.html.twig');
     }
 
-    public function deleteAction()
+    public function deleteAction($identifiant)
     {
-    	return new response('Deleted Client ! ');
+    	//return new response('Supression du Client ' . $identifiant);
+    	return $this->render('CustomerBundle:Default:delete.html.twig');
     }
 }
