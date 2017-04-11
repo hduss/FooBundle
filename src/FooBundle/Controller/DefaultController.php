a801f15b24a0942d0c1a40660b616dbeaccae03a
hduss <delmas.theo@laposte.net>
2017-04-11 10:25:31 +0200

    creation routing /foo /bar + controller fooAction barAction dans DaufaultController

--- src/FooBundle/Controller/DefaultController.php
+++ src/FooBundle/Controller/DefaultController.php
@@ -3,11 +3,24 @@
 namespace FooBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('FooBundle:Default:index.html.twig');
+    }
+
+
+    public function fooAction()
+    {
+    	return new response('FOO!!');
+    }
+
+
+    public function barAction()
+    {
+    	return new response('BAR!!!');
     }
 }
