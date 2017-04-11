0af55edb2844e52afdd53930ef2af391141dd74c
hduss <delmas.theo@laposte.net>
2017-04-11 11:44:27 +0200

    creation nouveau Bundle CustomerBundlet chainage des routing customer et {identifiant}+ creation des controller avec des return new Response

--- src/CustomerBundle/Controller/DefaultController.php
+++ src/CustomerBundle/Controller/DefaultController.php
@@ -3,11 +3,32 @@
 namespace CustomerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('CustomerBundle:Default:index.html.twig');
+    }
+
+    public function createAction()
+    {
+    	return new response('Created !!');
+    }
+
+    public function detailAction($identifiant)
+    {
+    	return new response('Detail du client ' . $identifiant);
+    }
+
+    public function updateAction()
+    {
+    	return new response('Update Client !');
+    }
+
+    public function deleteAction()
+    {
+    	return new response('Deleted Client ! ');
     }
 }
