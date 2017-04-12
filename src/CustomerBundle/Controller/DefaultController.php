b9f90c5ec0d31fd5ae257185c1cb29caa65328da
hduss <delmas.theo@laposte.net>
2017-04-12 13:25:38 +0200

    fin de la function delete + son formulaire + template et ajout d'une entitée adresse dans la BDD --> OK

--- src/CustomerBundle/Controller/DefaultController.php
+++ src/CustomerBundle/Controller/DefaultController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use CustomerBundle\Entity\Customer;
 use CustomerBundle\Form\CustomerType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\IsTrue;
 
 class DefaultController extends Controller
 {
@@ -51,67 +52,135 @@ class DefaultController extends Controller
 ## ----------------------------------------------------------  ##
 
 
-
-
-
     public function detailAction($identifiant)
     {
-
+// on recupere le repo sur la classe customer
     	$repo = $this
     		->getDoctrine()
     		->getRepository('CustomerBundle:Customer');
 
+
+//on recupere l'entitée par son identifiant
     	$customer = $repo->find($identifiant);
 
 
-
+// puis on affiche le template detail.html.twig avec comme parametre le customer pour pouvoir afficher ses valeurs
     	return $this->render('CustomerBundle:Default:detail.html.twig',
     	 [
     	 'customer' => $customer,
     	 ]);
     }
 
+
+## ----------------------------------------------------------
+
+
     public function updateAction(Request $request)
     {
 
+// on recupere l'identifiant du customer grace a request->attributes->get('identifiant')
     	$identifiant = $request->attributes->get('identifiant');
 
-
+// on initialise le repo
     	$repo = $this
     		->getDoctrine()
     		->getRepository('CustomerBundle:Customer');
 
+
+// on recupere l'entité par rapport a son id recupéré precedement
     	$customer = $repo->find($identifiant);
 
+
+// on crée un formulaire qui recupere les valeurs de l'entitée
         $form = $this
         	->createForm(new CustomerType(), $customer)
         	->add('submit', 'submit');
 
         $form->handleRequest($request);
 
+
+// si le formulaire et valide et soumis
         if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
             $em->persist($customer);
             $em->flush();
 
+// on redirige vers la homepage
             return $this->redirectToRoute('customer_homepage');
         }
 
 
-
-        return $this->render('CustomerBundle:Default:create.html.twig', [
+// ici ce sera la page sur laquelle on atterit avant la soumission du formulaire
+        return $this->render('CustomerBundle:Default:update.html.twig', [
             'form' => $form->createView(),
+            'customer' => $customer,
         ]);
     }
 
-    public function deleteAction($identifiant)
+
+## ----------------------------------------------------------
+
+    public function deleteAction(Request $request)
     {
 
-    	return $this->render('CustomerBundle:Default:delete.html.twig',
-    	 [
-    	 'identifiant' => $identifiant,
-    	 ]);
+    	$identifiant = $request->attributes->get('identifiant');
+
+        $repository = $this
+            ->getDoctrine()
+            ->getRepository('CustomerBundle:Customer');
+
+        $customer = $repository->find($identifiant);
+
+        if (null == $customer) {
+
+            throw $this->createNotFoundException(
+                'Command not found'
+                
+            );
+        }
+
+        $data =[
+
+        ];  
+
+        $form = $this->createFormBuilder()
+        	->add('confirm', 'checkbox', [
+        		'label' => 'Confirmer la supression',
+        		'required' => false,
+        		'constraints' => [
+        			new isTrue()]
+        		])
+
+        	->add('submit', 'submit')
+        	->getForm();
+
+        $form->handleRequest($request);
+
+
+        // 3. Si la case est cochée, supprimer le client
+        //Et rediriger vers la liste
+
+        if ($form->isSubmitted() && $form->isValid()) 
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($customer);
+            $em->flush();
+
+            $this->redirectToRoute('customer_homepage');
+        }
+
+        return $this->render('CustomerBundle:Default:delete.html.twig', [
+            'form' => $form->createView(),
+        ]);
+
+
+
     }
+
+
+
+## ----------------------------------------------------------
+
 
 
 
---
+++ src/CustomerBundle/Entity/Address.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace CustomerBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Address
+ */
+class Address
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $street;
+
+    /**
+     * @var int
+     */
+    private $postalCode;
+
+    /**
+     * @var string
+     */
+    private $city;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set street
+     *
+     * @param string $street
+     * @return Address
+     */
+    public function setStreet($street)
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    /**
+     * Get street
+     *
+     * @return string 
+     */
+    public function getStreet()
+    {
+        return $this->street;
+    }
+
+    /**
+     * Set postalCode
+     *
+     * @param integer $postalCode
+     * @return Address
+     */
+    public function setPostalCode($postalCode)
+    {
+        $this->postalCode = $postalCode;
+
+        return $this;
+    }
+
+    /**
+     * Get postalCode
+     *
+     * @return integer 
+     */
+    public function getPostalCode()
+    {
+        return $this->postalCode;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     * @return Address
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string 
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+}
